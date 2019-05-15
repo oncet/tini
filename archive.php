64f36bc868f7b4a3f3510a59d16d234fd93cf537
runonce <camilorivera86@gmail.com>
2019-05-15 12:19:48 -0300

    Show category description

--- archive.php
+++ archive.php
@@ -3,5 +3,6 @@
 $context = Timber::context();
 
 $context['posts'] = new Timber\PostQuery();
+$context['category'] = get_category(get_query_var('cat'));
 
 Timber::render(['archive.twig'], $context);
